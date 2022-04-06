7d73f78306cf6ffd0defffe66674ae8750b36add
Маматов Ыйманбек <77632341+MamatovY@users.noreply.github.com>
2022-04-06 18:43:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
     echo '&#10084; <a href="logout.php">Выход из приложения('.$_SESSION['username'] .') </a><br />';
   
   }else {
-    echo('<h2 class="login"> Вы вошли в приложение как Гость.</h2>');
+    echo('<h2 class="login"> Вы вошли в приложение как Гость!</h2>');
     echo '<h3>Mismatch - Where opposites attract!</h3>';
     echo '&#10084; <a href="login.php">Вход в приложение</a><br />';
     echo '&#10084; <a href="signup.php">Создание учетной записи</a><br />';
